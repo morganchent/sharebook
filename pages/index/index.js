52ead45006cd552353cda3b311442956247fb996
feiyun <feiyun@feiyundeMacBook-Pro.local>
2016-12-23 15:55:11 +0800

    scan isbn to search bookinfo

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    userInfo: {}
+  },
+  
+  onImageClick: function() {
+    wx.navigateTo({
+      url: '../detail/detail?isbn=' + '7505715666'
+    })
+  },
+  
+  onScanBtnClick: function() {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+        wx.navigateTo({
+          url: '../detail/detail?isbn=' + res.result
+        })
+      }
+    })
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
